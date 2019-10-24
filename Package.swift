98299eb8c68063ba4cbb68924c9edf2a307bc639
Oleksii Moiseenko <>
2019-10-24 14:21:35 +0300

    swinject version;

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     products: [.library(name: "RCKit", targets: ["RCKit"])],
     dependencies:[
         .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "5.0.0"),
-        .package(url: "https://github.com/Swinject/Swinject",  .branch("master")),
+        .package(url: "https://github.com/Swinject/Swinject",  from: "2.7.1"),
     ],
     targets: [
         .target(name: "RCKit", dependencies: ["RxSwift", "RxCocoa", "Swinject", "Reachability"])
