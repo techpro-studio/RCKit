9937517e5536a8cba038ad07e0a718a0fab4351c
alex <alex@techpro.studio>
2021-04-14 23:18:34 +0300

    package dep updated

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     platforms: [.iOS(SupportedPlatform.IOSVersion.v9)],
     products: [.library(name: "RCKit", targets: ["RCKit"])],
     dependencies:[
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "5.0.0"),
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "6.1.0"),
         .package(url: "https://github.com/Swinject/Swinject",  from: "2.7.1"),
     ],
     targets: [
